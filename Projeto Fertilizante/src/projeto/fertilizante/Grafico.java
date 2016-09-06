d0162672fe64dfdd3ad81c151ce4b82c25ca55a8
b165135 <bacs30@gmail.com>
2016-09-06 13:36:27 -0300

    Add files via upload

---
+++ Projeto Fertilizante/src/projeto/fertilizante/Grafico.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.fertilizante;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Grafico {
+    int id_grafico;
+}
---
+++ Projeto Fertilizante/src/projeto/fertilizante/Pais.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.fertilizante;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Pais {
+    String nome;
+    int codigo;
+    int valor_consumo;
+    char regiao;
+    String grupo_renda;
+    
+    
+    
+}
